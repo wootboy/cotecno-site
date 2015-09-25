42cbcc4270db4b6d559addab7e65f3ac115b0349
David Grass <Dgrassb@gmail.com>
2015-09-25 18:43:42 -0300

    pagina funcionando perfeco con partials con las imagenes en publico pero el menu no se pone transparente

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,41 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require vendor/modernizr/modernizr
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+
+
+//= require vendor/jquery.appear/jquery.appear
+    //= require vendor/jquery.easing/jquery.easing
+    //= require vendor/jquery-cookie/jquery-cookie
 //= require bootstrap-sprockets
-//= require_tree .
+    //= require vendor/common/common
+    //= require vendor/jquery.validation/jquery.validation.js
+    //= require vendor/jquery.stellar/jquery.stellar
+    //= require vendor/jquery.easy-pie-chart/jquery.easy-pie-chart
+    //= require vendor/jquery.gmap/jquery.gmap
+    //= require vendor/jquery.lazyload/jquery.lazyload
+    //= require vendor/isotope/jquery.isotope.js
+    //= require vendor/owlcarousel/owl.carousel
+    //= require vendor/jflickrfeed/jflickrfeed
+    //= require vendor/magnific-popup/jquery.magnific-popup
+    //= require vendor/vide/vide.js
+
+
+//= require theme
+    //= require vendor/rs-plugin/js/jquery.themepunch.tools.min.js
+    //= require vendor/rs-plugin/js/jquery.themepunch.revolution.min.js
+    //= require vendor/circle-flip-slideshow/js/jquery.flipshow
+    //= require views/view.home
+
+    //= require custom
+
+
+    //= require theme.init.js
+
+
+
+
+
